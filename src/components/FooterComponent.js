900f7dfd61feb9883d1bcce07a7d3a97c941c168
TejasviKashyap <teju1402@yahoo.in>
2021-07-19 02:00:43 +0530

    I'm doneeeeee

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -17,9 +17,7 @@ function Footer(props){
                 <div className="col-7 col-sm-5">
                     <h5>Our Address</h5>
                     <address>
-		              121, Clear Water Bay Road<br />
-		              Clear Water Bay, Kowloon<br />
-		              HONG KONG<br />
+                    #216 Bell Road <nr /> Whitefield <br /> Bangalore <br />
 		              <i className="fa fa-phone fa-lg"></i>: +852 1234 5678<br />
 		              <i className="fa fa-fax fa-lg"></i>: +852 8765 4321<br />
 		              <i className="fa fa-envelope fa-lg"></i>: <a href="mailto:confusion@food.net">
